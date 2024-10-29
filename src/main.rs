991f673be0e69e2832f774b1b13782e50981a675
Ryan Ogden <12yanogden@gmail.com>
2024-10-29 10:28:12 -0400

    finish guessing game, without custom crates

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::*;
 
 fn main() {
+    let secret_number = random_number_between(1, 100);
+
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!!".green());
+                break;
+            },
+        }
+    }
+}
+
+
+fn random_number_between(min: u32, max: u32) -> u32 {
+    return rand::thread_rng().gen_range(min..=max);
 }
